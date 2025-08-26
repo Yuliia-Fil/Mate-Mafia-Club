2319e01b7e3148360920ffe8629b4af19a8c6cde
Yulia Fil <filyulia.yf@gmail.com>
2025-08-26 21:09:27 +0300

    Add icons lib, remove node_modules from remote repo

--- client/src/components/RulesPage.tsx
+++ client/src/components/RulesPage.tsx
@@ -1,5 +1,35 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { rules } from "../constants";
+import { PageTitle } from "./PageTitle";
 import { RolesSlider } from "./RolesSlider/RolesSlider";
 
 export const RulesPage = () => {
-  return <RolesSlider />;
+  return (
+    <>
+      <RolesSlider />
+      <PageTitle path="custom" />
+      <div>
+        {rules.map((rule) => (
+          <Accordion key={rule.title}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={rule.title}
+              id={rule.title}
+            >
+              <span style={{ fontSize: "48px" }}>{rule.img}</span>
+              <Typography component="span">{rule.title}</Typography>
+              <Typography component="span">{rule.subtitle}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>{rule.description}</AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </>
+  );
 };
