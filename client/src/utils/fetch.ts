eb13c2da6da6a86d1006064065dc75b64becf0f9
Yulia Fil <filyulia.yf@gmail.com>
2025-09-19 11:12:04 +0300

    changed baseurl to remote server

--- client/src/utils/fetch.ts
+++ client/src/utils/fetch.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://mate-mafia-club-0mq7.onrender.com";
+// const BASE_URL_LOCAL = "http://127.0.0.1:8000";
 
 export const getData = <T>(
   path: string,
